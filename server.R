8b747ea8929b9b39ed8165aafcc8503ece015458
JimenaSM <jimena_simu@hotmail.com>
2016-12-14 18:01:44 -0600

    Tareas Estadística Computacional

--- server.R
+++ server.R
@@ -2,7 +2,8 @@
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
-# http://shiny.rstudio.com
+# 
+#http://shiny.rstudio.com
 #
 
 library(shiny)
